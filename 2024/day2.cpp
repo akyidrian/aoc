f61190bc25a18df6d84d82359d2461389f0ac2ba
akyidrian <1908921+akyidrian@users.noreply.github.com>
2024-12-03 12:12:46 +1300

    Refactoring day2

--- 2024/day2.cpp
+++ 2024/day2.cpp
@@ -1,20 +1,37 @@
+#include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <iterator>
+#include <span>
 #include <sstream>
 #include <ranges>
 #include <vector>
 
 using namespace std;
 
+bool increasing(int a, int b)
+{
+    return (a - b) <= -1 && (a - b) >= -3;
+}
+
+bool decreasing(int a, int b)
+{
+    return (a - b) >= 1 && (a - b) <= 3;
+}
+
+bool allIncreasing(span<const int>& levels)
+{
+    return ranges::adjacent_find(levels, not_fn(increasing)) == levels.end();
+}
+
+bool allDecreasing(span<const int>& levels)
+{
+    return ranges::adjacent_find(levels, not_fn(decreasing)) == levels.end();
+}
+
 bool part1(vector<int>& levels)
 {
-    auto increasing = [](int a, int b){
-        return (a - b) <= -1 && (a - b) >= -3;
-    };
-    auto decreasing = [](int a, int b){
-        return (a - b) >= 1 && (a - b) <= 3;
-    };
     auto incCounter = 0u;
     auto decCounter = 0u;
     for(size_t i = 1; i < levels.size(); ++i)
@@ -27,55 +44,27 @@ bool part1(vector<int>& levels)
     return incCounter==(levels.size()-1) || decCounter==(levels.size()-1);
 }
 
-bool allIncreasing(const vector<int>& levels)
-{
-    auto increasing = [](int a, int b){
-        return (a - b) <= -1 && (a - b) >= -3;
-    };
-    for(auto i = 1u; i < levels.size(); ++i)
-    {
-        if(!increasing(levels[i-1], levels[i]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
-bool allDecreasing(const vector<int>& levels)
-{
-    auto decreasing = [](int a, int b){
-        return (a - b) >= 1 && (a - b) <= 3;
-    };
-    for(auto i = 1u; i < levels.size(); ++i)
-    {
-        if(!decreasing(levels[i-1], levels[i]))
-        {
-            return false;
-        }
-    }
-    return true;
-}
 
 bool part2(const vector<int>& levels)
 {
-    if(allIncreasing(levels) || allDecreasing(levels))
+    span s(levels);
+    if(allIncreasing(s) || allDecreasing(s))
     {
         return true;
     }
     auto skip = 0u;
     while(skip < levels.size())
     {
-        vector<int> newLevels;
-        for(auto i = 0u; i < levels.size(); ++i)
+        // Dividing report into two halves around the skip point...
+        span left(levels.begin(), skip);
+        span right(levels.begin()+skip+1, levels.end());
+        auto leftRightIncreasing = !left.size() || !right.size() || increasing(left.back(), right.front());
+        auto leftRightDecreasing = !left.size() || !right.size() || decreasing(left.back(), right.front());
+        if(leftRightIncreasing && allIncreasing(left) && allIncreasing(right))
         {
-            if(i == skip)
-            {
-                continue;
-            }
-            newLevels.push_back(levels[i]);
+            return true;
         }
-        if(allIncreasing(newLevels) || allDecreasing(newLevels))
+        else if(leftRightDecreasing && allDecreasing(left) && allDecreasing(right))
         {
             return true;
         }
@@ -112,5 +101,5 @@ int main(int argc, char** argv)
         safePart2 += part2(levels);
     }
     cout << "Part 1 safe reports: " << safePart1 << "\n";
-    cout << "Part 2 safe reports:" << safePart2 << "\n";
+    cout << "Part 2 safe reports: " << safePart2 << "\n";
 }
