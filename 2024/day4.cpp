7d2564f2108a61471f3ced4c503dbc9e8f99ef67
akyidrian <1908921+akyidrian@users.noreply.github.com>
2024-12-05 01:07:36 +1300

    Completed 2024 day4 part 2

--- 2024/day4.cpp
+++ 2024/day4.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-unsigned int xmas(int row, int col, const vector<string>& lines)
+unsigned int part1(int row, int col, const vector<string>& lines)
 {
     constexpr char x[] = "XMAS";
     constexpr int xLen = sizeof(x)-1;
@@ -81,6 +81,30 @@ unsigned int xmas(int row, int col, const vector<string>& lines)
     return count;
 }
 
+unsigned int part2(int row, int col, const vector<string>& lines)
+{
+    const int rows = lines.size();
+    const int cols = lines[0].size();
+
+    auto count = 0u;
+    const auto left = col-1 >= 0;
+    const auto right = col+1 < cols;
+    const auto up = row-1 >= 0;
+    const auto down = row+1 < rows;
+    if(lines[row][col] != 'A' || !left || !right || !up || !down)
+    {
+        return 0;
+    }
+
+    auto nw = lines[row-1][col-1];
+    auto se = lines[row+1][col+1];
+    auto sw = lines[row+1][col-1];
+    auto ne = lines[row-1][col+1];
+    auto downwardsDiagonal = (nw == 'M' && se == 'S') || (nw == 'S' && se == 'M');
+    auto upwardsDiagonal = (sw == 'M' && ne == 'S') || (sw == 'S' && ne == 'M');
+    return downwardsDiagonal && upwardsDiagonal;
+}
+
 int main(int argc, char** argv)
 {
     stringstream example(
@@ -108,13 +132,16 @@ MXMXAXMASX
         lines.push_back(line);
     }
 
-    auto count = 0u;
+    auto countPart1 = 0u;
+    auto countPart2 = 0u;
     for(auto row = 0u; row < lines.size(); ++row)
     {
         for(auto col = 0u; col < lines[0].size(); ++col)
         {
-            count += xmas(row, col, lines);
+            countPart1 += part1(row, col, lines);
+            countPart2 += part2(row, col, lines);
         }
     }
-    cout << "Total count: " << count << "\n";
+    cout << "Total count (part 1): " << countPart1 << "\n";
+    cout << "Total count (part 2): " << countPart2 << "\n";
 }
