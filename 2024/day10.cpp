45853c733346cb0f4eb57be2c3bbb9f1085c03a5
akyidrian <1908921+akyidrian@users.noreply.github.com>
2024-12-11 13:58:47 +1300

    Refactoring day10 2024

--- 2024/day10.cpp
+++ 2024/day10.cpp
@@ -29,7 +29,7 @@ struct Position
         return std::tie(y, x) < std::tie(other.y, other.x);
     }
 
-    bool inBounds(const int xSize, const int ySize) const
+    bool inBounds(const int ySize, const int xSize) const
     {
         return x >= 0 && y >= 0 && x < xSize && y < ySize;
     }
@@ -62,9 +62,87 @@ enum class Direction : uint8_t
 constexpr array<Position, 4> directionMap{{/*up*/{-1, 0}, /*down*/ {1, 0}, /*left*/ {0, -1}, /*right*/ {0, 1}}};
 using direction_t = std::underlying_type_t<Direction>;
 
-unsigned int getHeight(const vector<string>& topographicMap, const Position& pos)
+unsigned int getHeight(const vector<string>& topoMap, const Position& pos)
 {
-    return topographicMap[pos.y][pos.x] - '0';
+    return topoMap[pos.y][pos.x] - '0';
+}
+
+queue<Position> getTrailheads(const vector<string>& topoMap)
+{
+    const auto ySize = topoMap.size();
+    const auto xSize = topoMap[0].size();
+    queue<Position> trailheads;
+    for(int y = 0; y < ySize; ++y)
+    {
+        for(int x = 0; x < xSize; ++x)
+        {
+            Position p{y,x};
+            if(getHeight(topoMap, p) == 0) // trailheads are at height 0
+            {
+                trailheads.emplace(p);
+            }
+        }
+    }
+    return trailheads;
+}
+
+void stepTrailPath(queue<Position>& trailPositions,
+                   unordered_set<Position, Position::Hash>& trailEndsSet,
+                   vector<Position>& trailEndsVec,
+                   const vector<string>& topoMap)
+{
+    const auto& current = trailPositions.front();
+    trailPositions.pop();
+    const auto ySize = topoMap.size();
+    const auto xSize = topoMap[0].size();
+    const auto currentHeight = getHeight(topoMap, current);
+    for(const auto& dir : directionMap)
+    {
+        const auto next = current + dir;
+        if(next.inBounds(ySize, xSize))
+        {
+            const auto nextHeight = getHeight(topoMap, next);
+            const auto goingUp = (nextHeight - currentHeight) == 1;
+            if(goingUp && nextHeight == 9) // reached the trail end?
+            {
+                trailEndsSet.emplace(next);
+                trailEndsVec.emplace_back(next);
+            }
+            else if(goingUp)
+            {
+                trailPositions.emplace(next);
+            }
+        }
+    }
+}
+
+
+auto calculateTotal(const vector<string>& topoMap, queue<Position> trailheads)
+{
+    pair<unsigned int, unsigned int> total; // score, rating
+    while(!trailheads.empty())
+    {
+        // Possible positions between heads and tails
+        queue<Position> trailPositions;
+        trailPositions.push(trailheads.front());
+        trailheads.pop();
+
+        // For score - we use the unordered_set to find distinct trail ends.
+        unordered_set<Position, Position::Hash> trailtailsSet;
+
+        // For rating - we use vector to store duplicate trail ends, which may
+        // represent more than one distinct path used to reach a trail end.
+        vector<Position> trailtailsVec;
+
+        stepTrailPath(trailPositions, trailtailsSet, trailtailsVec, topoMap);
+        while(!trailPositions.empty())
+        {
+            stepTrailPath(trailPositions, trailtailsSet, trailtailsVec, topoMap);
+        }
+        total.first += trailtailsSet.size();
+        total.second += trailtailsVec.size();
+    }
+    return total;
 }
 
 int main(int argc, char** argv)
@@ -91,86 +169,8 @@ int main(int argc, char** argv)
         topoMap.emplace_back(line);
     }
 
-    const auto ySize = topoMap.size();
-    const auto xSize = topoMap[0].size();
-    queue<Position> trailheads;
-    for(int y = 0; y < ySize; ++y)
-    {
-        for(int x = 0; x < xSize; ++x)
-        {
-            Position p{y,x};
-            if(getHeight(topoMap, p) == 0)
-            {
-                trailheads.emplace(p);
-            }
-        }
-    }
-
-//    auto stepTrailPath = [&topoMap](queue<Position>& trailPositions, unordered_set<Position, Position::Hash>& trailEnd, const Position& current)
-//    {
-//        const auto ySize = topoMap.size();
-//        const auto xSize = topoMap[0].size();
-//        const auto currentHeight = getHeight(topoMap, current);
-//        for(const auto& dir : directionMap)
-//        {
-//            const auto next = current + dir;
-//            if(next.inBounds(xSize, ySize))
-//            {
-//                const auto nextHeight = getHeight(topoMap, next);
-//                const auto goingUp = (nextHeight - currentHeight) == 1;
-//                if(goingUp && nextHeight == 9)
-//                {
-//                    // There may be more than one path to a trail end
-//                    // which is why we use an unordered_set
-//                    trailEnd.emplace(next);
-//                }
-//                else if(goingUp)
-//                {
-//                    trailPositions.emplace(next);
-//                }
-//            }
-//        }
-//    };
-
-    auto stepTrailPath = [&topoMap](queue<Position>& trailPositions, vector<Position>& trailEnd, const Position& current)
-    {
-        const auto ySize = topoMap.size();
-        const auto xSize = topoMap[0].size();
-        const auto currentHeight = getHeight(topoMap, current);
-        for(const auto& dir : directionMap)
-        {
-            const auto next = current + dir;
-            if(next.inBounds(xSize, ySize))
-            {
-                const auto nextHeight = getHeight(topoMap, next);
-                const auto goingUp = (nextHeight - currentHeight) == 1;
-                if(goingUp && nextHeight == 9)
-                {
-                    // There may be more than one path to a trail end
-                    // which is why we use an unordered_set
-                    trailEnd.emplace_back(next);
-                }
-                else if(goingUp)
-                {
-                    trailPositions.emplace(next);
-                }
-            }
-        }
-    };
-    auto totalScore = 0u;
-    while(!trailheads.empty())
-    {
-        queue<Position> trailPositions;
-        //unordered_set<Position, Position::Hash> trailtail;
-        vector<Position> trailtail;
-        stepTrailPath(trailPositions, trailtail, trailheads.front());
-        while(!trailPositions.empty())
-        {
-            stepTrailPath(trailPositions, trailtail, trailPositions.front());
-            trailPositions.pop();
-        }
-        totalScore += trailtail.size();
-        trailheads.pop();
-    }
+    auto trailheads = getTrailheads(topoMap);
+    const auto [totalScore, totalRating] = calculateTotal(topoMap, trailheads);
     cout << totalScore << endl;
+    cout << totalRating << endl;
 }
