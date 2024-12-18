898cfe5799580a9114c67db10ce1c213df1c4df8
akyidrian <1908921+akyidrian@users.noreply.github.com>
2024-12-18 18:11:58 +1300

    Refactoring day17 some more

--- 2024/day17.cpp
+++ 2024/day17.cpp
@@ -8,94 +8,201 @@
 
 using namespace std;
 
-auto registerA = 0ul, registerB = 0ul, registerC = 0ul;
-vector<unsigned long> program;
-auto instructionPointer = 0ul;
-stringstream out;
-
-auto combo(unsigned long operand)
+enum class Opcode : uint8_t
 {
-    switch(operand)
+    Adv = 0,
+    Bxl,
+    Bst,
+    Jnz,
+    Bxc,
+    Out,
+    Bdv,
+    Cdv
+};
+
+struct Computer
+{
+    unsigned long a = 0ul;
+    unsigned long b = 0ul;
+    unsigned long c = 0ul;
+    vector<uint8_t> program;
+    long p = 0l; // 'instruction pointer'
+    stringstream out;
+
+    Computer() = delete;
+    Computer(unsigned long a, unsigned long b, unsigned long c, const vector<uint8_t>& program)
+    : a(a), b(b), c(c), program(program) {}
+
+    Computer(const Computer& other)
+        : a(other.a),
+          b(other.b),
+          c(other.c),
+          program(other.program),
+          p(other.p)
     {
-        case(0):
-        case(1):
-        case(2):
-        case(3):
-            return operand;
-        case(4):
-            return registerA;
-        case(5):
-            return registerB;
-        case(6):
-            return registerC;
-        case(7):
-            throw runtime_error("Reserved and will not appear in valid programs");
+        out.str(other.out.str());
+        out.clear();
+        out.setstate(other.out.rdstate());
     }
-    return 0ul;
-}
 
-void instruction(unsigned long op, unsigned long operand)
-{
-    switch(op)
+    unsigned long combo(unsigned long operand)
     {
-        case(0): // adv
+        switch(operand)
         {
-            registerA /= pow(2, combo(operand));
-            instructionPointer += 2ul;
-            return;
+            case(0):
+            case(1):
+            case(2):
+            case(3):
+                return operand;
+            case(4):
+                return a;
+            case(5):
+                return b;
+            case(6):
+                return c;
+            case(7):
+                throw runtime_error("Reserved and will not appear in valid programs");
         }
-        case(1): // bxl
-        {
-            registerB ^= operand;
-            instructionPointer += 2ul;
-            return;
-        }
-        case(2): // bst
-        {
-            registerB = combo(operand) % 8;
-            instructionPointer += 2ul;
-            return;
-        }
-        case(3): // jnz
-        {
-            if(!registerA)
+        throw runtime_error("Invalid operand");
+    }
+
+    void execute(Opcode opcode, unsigned long operand)
+    {
+        auto comboOperand = combo(operand);
+        if      (opcode == Opcode::Adv) a /= pow(2, comboOperand);
+        else if (opcode == Opcode::Bxl) b ^= operand;
+        else if (opcode == Opcode::Bst) b = comboOperand % 8;
+        else if (opcode == Opcode::Jnz) {
+            if(a)
             {
-                instructionPointer += 2ul;
+                p = operand;
                 return;
             }
-            instructionPointer = operand;
-            return;
         }
-        case(4): // bxc
+        else if (opcode == Opcode::Bxc) b ^= c; // Ignore operand (legacy reasons)
+        else if (opcode == Opcode::Out)
         {
-            // Ignore operand (legacy reasons)
-            registerB ^= registerC;
-            instructionPointer += 2ul;
-            return;
+                if(out.str().size() > 0)
+                {
+                    out << ',';
+                }
+                out << comboOperand % 8;
         }
-        case(5): // out
+        else if (opcode == Opcode::Bdv) b = a / pow(2, comboOperand);
+        else if (opcode == Opcode::Cdv) c = a / pow(2, comboOperand);
+        p += 2ul;
+    }
+
+    string run()
+    {
+        for(auto it = program.begin(); it != program.end();)
         {
-            if(out.str().size() > 0)
+            auto op = *it;
+            auto operand = *(it+1);
+            execute(static_cast<Opcode>(op), operand);
+            it = program.begin() + p;
+        }
+        return out.str();
+    }
+
+    // FIXME: This works but makes multiple assumptions based on the program input...
+    bool solve(const vector<uint8_t>& program, long p, unsigned long reg)
+    {
+        if(p < 0)
+        {
+            cout << reg << endl;
+            return true;
+        }
+        for(auto d : views::iota(0, 8))
+        {
+            auto a = reg << 3 | d;
+            auto b = 0ul;
+            auto c = 0ul;
+            auto out = 0ul;
+            auto i = 0l;
+            while(i < program.size())
             {
-                out << ',';
+                auto o = 0ul;
+                if      (program[i+1] <= 3) o = program[i+1];
+                else if (program[i+1] == 4) o = a;
+                else if (program[i+1] == 5) o = b;
+                else if (program[i+1] == 6) o = c;
+
+                if      (program[i] == 0) a >>= o;
+                else if (program[i] == 1) b ^= program[i+1];
+                else if (program[i] == 2) b = o & 7;
+                else if (program[i] == 3)
+                {
+                    // TODO: We don't ever hit this case here...
+                    //if(a != 0)
+                    //{
+                    //    i = program[i+1] - 2;
+                    //}
+                }
+                else if (program[i] == 4) b ^= c;
+                else if (program[i] == 5)
+                {
+                    out = o & 7;
+                    break;
+                }
+                else if (program[i] == 6) b = a >> o;
+                else if (program[i] == 7) c = a >> o;
+                i += 2;
             }
-            out << combo(operand) % 8;
-            instructionPointer += 2ul;
-            return;
+            if(out == program[p] && solve(program, p-1, reg << 3 | d))
+            {
+                return true;
+            }
         }
-        case(6): // bdv
+        return false;
+    }
+
+    void solve()
+    {
+        solve(program, program.size()-1, 0);
+    }
+};
+
+Computer parse(istream& input)
+{
+    auto registerA = 0ul;
+    auto registerB = 0ul;
+    auto registerC = 0ul;
+    vector<uint8_t> program;
+
+    regex registerRegex(R"(Register\s([A-C]):\s(\d+))");
+    regex programRegex(R"(Program:\s((\d+,?)+))");
+    string line;
+    smatch match;
+
+    getline(input, line);
+    if(regex_match(line, match, registerRegex))
+    {
+        registerA = stoul(match[2]);
+    }
+    getline(input, line);
+    if(regex_match(line, match, registerRegex))
+    {
+        registerB = stoul(match[2]);
+    }
+    getline(input, line);
+    if(regex_match(line, match, registerRegex))
+    {
+        registerC = stoul(match[2]);
+    }
+    getline(input, line); // Skip the \n
+    getline(input, line);
+    if(regex_match(line, match, programRegex))
+    {
+        string programStr = match[1].str();
+        for (auto&& range : programStr | views::split(','))
         {
-            registerB = registerA / pow(2, combo(operand));
-            instructionPointer += 2ul;
-            return;
-        }
-        case(7): // cdv
-        {
-            registerC = registerA / pow(2, combo(operand));
-            instructionPointer += 2ul;
-            return;
+            string_view sv(&*range.begin(), ranges::distance(range));
+            program.emplace_back(stoul(string(sv)));
         }
     }
+
+    return {registerA, registerB, registerC, program};
 }
 
 int main(int argc, char** argv)
@@ -121,58 +228,8 @@ Program: 0,3,5,4,3,0)");
         return 1;
     }
     auto& input = file;
-    string line;
-    regex registerRegex(R"(Register\s([A-C]):\s(\d+))");
-    regex programRegex(R"(Program:\s((\d+,?)+))");
-    smatch match;
-    getline(input, line);
-    auto registerARegex = 0ul;
-    auto registerBRegex = 0ul;
-    auto registerCRegex = 0ul;
-    if(regex_match(line, match, registerRegex))
-    {
-        registerARegex = stoul(match[2]);
-    }
-    getline(input, line);
-    if(regex_match(line, match, registerRegex))
-    {
-        registerBRegex = stoul(match[2]);
-    }
-    getline(input, line);
-    if(regex_match(line, match, registerRegex))
-    {
-        registerCRegex = stoul(match[2]);
-    }
-    getline(input, line); // \n
-    getline(input, line);
-    string programStr;
-    if(regex_match(line, match, programRegex))
-    {
-        programStr = match[1].str();
-        for (auto&& range : programStr | views::split(','))
-        {
-            string_view sv(&*range.begin(), ranges::distance(range));
-            program.emplace_back(stoul(string(sv)));
-        }
-    }
-    registerA = registerARegex;
-    registerB = registerBRegex;
-    registerC = registerCRegex;
-    cout << "Register A: " << registerA << endl;
-    cout << "Register B: " << registerB << endl;
-    cout << "Register C: " << registerC << endl;
-    cout << "Program   : " << programStr << endl;
 
-    for(auto it = program.begin(); it != program.end();)
-    {
-        auto op = *it;
-        auto operand = *(it+1);
-        instruction(op, operand);
-        it = program.begin() + instructionPointer;
-    }
-    cout << "Register A: " << registerA << endl;
-    cout << "Register B: " << registerB << endl;
-    cout << "Register C: " << registerC << endl;
-    cout << "Out       : "<< out.str() << endl;
-
+    auto computer = parse(input);
+    cout << "Part 1: " << computer.run() << endl;
+    computer.solve(); // Will print part 2
 }
